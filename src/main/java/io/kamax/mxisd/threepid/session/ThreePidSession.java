6e7e36a30236da6a0306a22545ccf4a211a5c7de
Chagai Friedlander <chagai.friedlander@aarenet.com>
2020-09-18 12:22:14 +0200

    no message

--- src/main/java/io/kamax/mxisd/threepid/session/ThreePidSession.java
+++ src/main/java/io/kamax/mxisd/threepid/session/ThreePidSession.java
@@ -63,7 +63,7 @@ public class ThreePidSession implements IThreePidSession {
         timestamp = Instant.ofEpochMilli(dao.getCreationTime());
         isValidated = dao.getValidated();
         if (isValidated) {
-            validationTimestamp = Instant.ofEpochMilli(dao.getCreationTime());
+            validationTimestamp = Instant.ofEpochMilli(dao.getValidationTime());
         }
 
         isRemote = dao.isRemote();
